351cd3af422fbc14ff234b38b3be0b5c5c30835d
Delina Dvorzhetskaya <delina.dvorzhetskaya@gmail.com>
2021-11-23 01:26:26 +0300

    Initial Commit

---
+++ Reviews/ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewsApp.swift
+//  Reviews
+//
+//  Created by Дэлина Дворжецкая on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
